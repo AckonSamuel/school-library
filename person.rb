725d1a0dbf42de62d6b3bf8b0bc33b78ba4a3900
Samuel Ackon <ackons433@gmail.com>
2022-09-27 03:57:17 +0000

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,23 +1,24 @@
 class Person
-    def initialize(name  = "Unknown", age, parent_permission = true )
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-    end
+    @parent_permission = parent_permission
+  end
 
-    attr_accessor :age, :name
+  attr_accessor :age, :name
 
-    attr_reader :id
+  attr_reader :id
 
-    def can_use_services?
-        @parent_permission = parent_permission
-        is_of_age? == true || @parent_permission == true
-    end
+  def can_use_services?
+    is_of_age? == true || @parent_permission == true
+  end
 
-    private
+  private
 
-    def is_of_age?
-        @age >= 18
-    end
-
+  # rubocop:disable Naming/PredicateName
+  def is_of_age?
+    # rubocop:enable Naming/PredicateName
+    @age >= 18
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,14 @@
-require './person.rb'
+require './person'
 
 class Teacher < Person
-    def initialize(specilization)
-        super()
-        @specilization = specilization
-    end
+  def initialize(specilization)
+    super()
+    @specilization = specilization
+  end
 
-    def is_of_age?
-        true
-    end
+  # rubocop:disable Naming/PredicateName
+  def is_of_age?
+    # rubocop:enable Naming/PredicateName
+    true
+  end
 end
