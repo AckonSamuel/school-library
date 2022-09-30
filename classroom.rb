40c8cefdd73a07bdb8fb4ef7ba3e764d1ced83c5
Samuel Ackon <ackons433@gmail.com>
2022-09-30 10:38:57 +0000

    Create the classroom has many student relationship

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,14 @@
 class Classroom
     attr_accessor :label
+    attr_reader :students
 
     def initialize(label)
         @label = label
+        @students = []
+    end
+
+    def add_student(student)
+        @students.push(student)
+        student.owner = self
     end
 end
