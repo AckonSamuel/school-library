e121b6887d8b8b8576a0823391923bb54ac534eb
Samuel Ackon <ackons433@gmail.com>
2022-09-30 11:26:27 +0000

    Fix linter errors

--- rental.rb
+++ rental.rb
@@ -1,14 +1,14 @@
 class Rental
-    attr_accessor :date
-    attr_reader :book
+  attr_accessor :date
+  attr_reader :book, :person
 
-    def initialize(date, book)
-        @date = date
+  def initialize(date, book, person)
+    @date = date
 
-        @book = book
-        book.rentals << self
+    @book = book
+    book.rentals << self
 
-        @person = person
-        person.rentals << self
-    end
+    @person = person
+    person.rentals << self
+  end
 end
