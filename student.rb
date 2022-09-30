d4d318272f3aac37e9f01b4672408d6ef8e954b1
Samuel Ackon <ackons433@gmail.com>
2022-09-30 11:01:05 +0000

    Create book class, setters and getters for instance variables

--- student.rb
+++ student.rb
@@ -1,9 +1,15 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom)
     super()
     @classroom = classroom
+  end
+
+  def classroom(room = @classroom)
+    room.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
