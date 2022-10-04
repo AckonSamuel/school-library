d5408953cc2a25b80550d5ebc2ed21c41abadaa3
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-04 14:33:58 +0100

    refactor main.rb, create seperate class for options

--- main.rb
+++ main.rb
@@ -1,22 +1,14 @@
 require_relative 'app'
+require_relative 'options'
 
 APP = App.new
+OPTION = Option.new
+
 
 class Main
-  def options
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
-
   def display_menu # rubocop:disable Metrics/CyclomaticComplexity
-    options
-    option = gets.chomp.to_i
+    OPTION.option
+    user_input = gets.chomp.to_i
     case option
     when 1
       APP.list_books
